40996adaa01160ed385134040ee47aafd06cd0a8
ralfaro <rodri.alfaro16@gmail.com>
2025-06-22 08:21:16 -0600

    :sparkles: Characters Cards and Alerts

--- src/async_function.js
+++ src/async_function.js
@@ -1,11 +1,44 @@
 import axios from "axios";
 export async function characters(params) {
     try {
-    let response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${params.name}&status=${params.status}&gender=${params.gender}`);
-    
-    console.log(response.data)
+        //if we've already got cards inside the card container, we need to clean it each time we filter
+        let alertContainer = document.querySelector("#alert-container"); //
+        let cardContainer = document.querySelector("#card-container");
+        cardContainer.innerHTML = ''; // Clear previous cards
+        alertContainer.innerHTML = ''; // Clear previous alerts
+
+        //here we obtain the data from the API
+        let response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${params.name}&status=${params.status}&gender=${params.gender}`);
+        let data = response.data.results;
+
+        //Width the data that we've gotten, we procced to create the card
+        data.map(character => {
+            let card = document.createElement('div');
+            card.className = 'card';
+            card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+            <p>Status: <span class="dot"  data-status="${character.status}"></span> ${character.status}</p>
+            <p>gender: ${character.gender}</p>
+            <p>Species: ${character.species}</p>
+            <p>Location: ${character.location.name}</p>`;
+            cardContainer.appendChild(card); //We end up adding the cards to the container we created above 
+        })
 
     } catch (error) {
-        console.log(`Error fetching characters: ${error}`);
+        // Handle errors and display an alert message
+        let alertContainer = document.querySelector("#alert-container");
+        let cardContainer = document.querySelector("#card-container");
+        let alert = document.createElement('div');
+        alert.className = 'alert';
+        if (error.response.status === 404) {
+            alert.innerHTML = `<p>No characters found with the given filters.</p>`;
+            cardContainer.innerHTML = ''; // Clear previous cards
+        } else {
+            alert.innerHTML = `<p>Error fetching data: ${error.message}</p>`;
+            cardContainer.innerHTML = ''; // Clear previous cards
+        }
+
+        alertContainer.appendChild(alert); // Append the alert to the card container
     }
 }
--- src/gallery.js
+++ src/gallery.js
@@ -1,9 +1,10 @@
 import { characters } from './async_function.js';
-export function gallery(){
+export function gallery() {
 
     //Here we create the form to filter the characters from the rick and morty API
     const galleryElement = document.createElement('div');
     galleryElement.className = 'main-container';
+    galleryElement.id = 'main-container'
     galleryElement.innerHTML = `
     <form id="character-form" class="form-style">
         <label for="search">Search Character:</label>
@@ -31,22 +32,33 @@ export function gallery(){
     // Append the gallery element to the app div inside the index.html file
     document.querySelector("#app").appendChild(galleryElement);
 
+    // We create a container for the alert messages
+    let alertContainer = document.createElement('div');
+    alertContainer.className = 'alert-container';
+    alertContainer.id = 'alert-container';
+    document.querySelector('#main-container').appendChild(alertContainer);
+
+    //We create the container of the cards here 
+    let div = document.createElement('div')
+    div.className = 'card-container';
+    div.id = 'card-container'
+    document.querySelector('#main-container').appendChild(div);
+
 
     // Add an event listener to the form to handle the search functionality
     document.getElementById("character-form").addEventListener('submit', (e) => {
-    e.preventDefault();
+        e.preventDefault();
 
-    //we get filter's values
-    let name = document.getElementById("name").value;
-    let status = document.getElementById("status").value;
-    let gender = document.getElementById("gender").value; 
+        //we get filter's values
+        let name = document.getElementById("name").value;
+        let status = document.getElementById("status").value;
+        let gender = document.getElementById("gender").value;
 
-    //we sanitize the input values
-    name = name.trim().toLowerCase();
-        
+        //we sanitize the input values
+        name = name.trim().toLowerCase();
 
-    // Call the characters function with the filter values
-    characters({name,status,gender})
+        // Call the characters function with the filter values
+        characters({ name, status, gender })
 
     })
 }
